dd999340c2496052f615b3e6a68abfd01339ad62
do.hoang.than <do.hoang.than@sun-asterisk.com>
2023-01-30 21:28:45 +0700

    add test assets

---
+++ Assets/Scripts/Test/ButtonTest.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WeAreProStars.Core.Manage.UI.Template;
+using MEC;
+
+public class ButtonTest : ButtonTemplate
+{
+    [SerializeField] Text title;
+
+    public override IEnumerator<float> Activate()
+    {
+        yield return Timing.WaitUntilTrue(() => this.initialized);
+        Timing.RunCoroutine(base.Activate());
+        Debug.Log("Activate");
+    }
+
+    public override IEnumerator<float> OnClick()
+    {
+        yield return Timing.WaitUntilTrue(() => this.initialized);
+        Timing.RunCoroutine(base.OnClick());
+        Debug.Log("Click");
+    }
+
+    public override IEnumerator<float> OnPostAdded_SetupUI<T>(T data, GameObject entity)
+    {
+        yield return Timing.WaitUntilTrue(() => this.initialized);
+        FakeData convert = data as FakeData;
+        this.title.text = convert.title;
+    }
+}
---
+++ Assets/Scripts/Test/ScrollViewManagerTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using WeAreProStars.Core.Manage;
+using MEC;
+
+public class ScrollViewManagerTest : MonoBehaviour
+{
+    public List<FakeData> datas = new List<FakeData>()
+    {
+        new FakeData("ABC"),
+        new FakeData("DEF")
+    };
+
+    private void Start()
+    {
+        Timing.RunCoroutine(_Start());
+    }
+
+    private IEnumerator<float> _Start()
+    {
+        var content = GetComponent<UIContentAbstract>();
+        if (content == null) yield break;
+        yield return Timing.WaitUntilTrue(() => content.initialized);
+        datas.ForEach(data => content.AddItem<FakeData>(data));
+    }
+}
+
+[Serializable]
+public class FakeData
+{
+    public string title;
+
+    public FakeData(string title)
+    {
+        this.title = title;
+    }
+}
